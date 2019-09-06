9efdbffa977bb46422be8ab651b1e344b11bc5db
MahmudulAlam <>
2019-09-06 15:59:05 +0600

    update

--- correlation.m
+++
@@ -1,7 +0,0 @@
-function return_value = correlation(x,y) 
-    n = length(x) ;
-    a = n .* sum(x.*y) - sum(x) .* sum(y) ;
-    b = n .* sum(x.^2) - (sum(x)).^2 ;
-    c = n .* sum(y.^2) - (sum(y)).^2 ;
-    return_value = (a./sqrt(b.*c)) ;
-end
--- variance.m
+++
@@ -1,4 +0,0 @@
-function return_value = variance(x)
-    mean_value = sum(x) / length(x) ;
-    return_value = (sum((x-mean_value).^2))/length(x) ;
-end
